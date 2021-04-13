eb0537df4f26804614368f24f83b51c241b1f6f1
Dhayanandan <t.dhayanandan@gmail.com>
2021-04-13 23:26:38 +0800

    teller applicaion initial commit

---
+++ tellerapplication/src/main/java/com/rest/ncs/tellerapplication/TellerapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.rest.ncs.tellerapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TellerapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TellerapplicationApplication.class, args);
+	}
+
+}
